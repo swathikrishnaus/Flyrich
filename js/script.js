0959059909370229aa32d8cf49d1624e50f5c707
swathikrishnaus <swathikrishnaus2015@gmail.com>
2023-01-28 18:45:14 +0530

    added

--- js/script.js
+++ js/script.js
@@ -5,9 +5,14 @@
 $(document).scroll(function () {
     var scroll = $(this).scrollTop();
     if (scroll > 50)
+    {
         $(".navbar").addClass("nav-shadow")
+    }
+       
     else
-        $(".navbar").removeClass("nav-shadow")
+        {
+            $(".navbar").removeClass("nav-shadow")
+        }
 })
 
 // -------------------nav toggle start--------------------
