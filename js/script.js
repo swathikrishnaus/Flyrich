1a5f6302cd266eccca1142f177d68c92cf37013f
swathikrishnaus <swathikrishnaus2015@gmail.com>
2023-01-28 16:05:14 +0530

    added

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+
+// -------------------nav toggle start--------------------
+
+$(document).scroll(function () {
+    var scroll = $(this).scrollTop();
+    if (scroll > 50)
+        $(".navbar").addClass("nav-shadow")
+    else
+        $(".navbar").removeClass("nav-shadow")
+})
+
+// -------------------nav toggle start--------------------
